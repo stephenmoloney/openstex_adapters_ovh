16bea1cb278c30142a2996bdc1db03ecfe280261
Stephen Moloney <stephen@stephenmoloney.com>
2017-03-17 16:14:19 +0000

    prepare mix file for release

--- mix.exs
+++ mix.exs
@@ -10,6 +10,10 @@ defmodule OpenstexAdaptersOvh.Mixfile do
      elixir: @elixir_versions,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     source_url: "https://github.com/stephenmoloney/openstex_adapters_ovh",
+     description: description(),
+     package: package(),
+     docs: docs(),
      deps: deps()
     ]
   end
@@ -26,7 +30,35 @@ defmodule OpenstexAdaptersOvh.Mixfile do
     [
       {:httpipe_adapters_hackney, ">= 0.10.0"},
       {:ex_ovh, "~> 0.3"},
-      {:openstex, "~> 0.3"}
+      {:openstex, "~> 0.3"},
+
+      # dev deps
+      {:markdown, github: "devinus/markdown", only: [:dev]},
+      {:ex_doc,  "~> 0.14", only: [:dev]},
     ]
   end
+
+
+  defp description() do
+    ~s"""
+    An adapter for the openstex library for making calls to the Openstack compliant OVH API.
+    """
+  end
+
+  defp package do
+    %{
+      licenses: ["MIT"],
+      maintainers: ["Stephen Moloney"],
+      links: %{ "GitHub" => "https://github.com/stephenmoloney/openstex"},
+      files: ~w(lib mix.exs CHANGELOG* README* LICENCE*)
+     }
+  end
+
+  defp docs() do
+    [
+    main: "Openstex.Adapters.Ovh",
+    extras: []
+    ]
+  end
+
 end
